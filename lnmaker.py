6e5127e8503d4ca45a74d8afdc3badaa6c905cce
meugr <meugr32@gmail.com>
2020-04-11 22:04:23 +0300

    create lnmaker.py

---
+++ lnmaker.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Usage: lnmaker.py dir_with_bins dir_with_symlinks"""
+
+from subprocess import Popen, PIPE, STDOUT
+from typing import List
+
+import os
+import sys
+
+
+def run_cmd(cmd: str) -> List[str]:
+    val = Popen(cmd, shell=True, stdout=PIPE, stderr=STDOUT)
+    out, _ = val.communicate()
+    return out.decode().rstrip('\n').split('\n')
+
+
+if __name__ == '__main__':
+    for d, _, files in os.walk(sys.argv[1]):
+        for f in files:
+            path_to_bin = os.path.join(d, f)
+            path_to_ln = os.path.join(sys.argv[2], f)
+            run_cmd(f'ln -s {path_to_bin} {path_to_ln}')
+        break
+
