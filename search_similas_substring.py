1706efae6b92f53c5e63ab809ab24125767a67f3
meugr <meugr32@gmail.com>
2020-06-17 23:30:19 +0300

    upd

--- search_similas_substring.py
+++ search_similas_substring.py
@@ -4,9 +4,15 @@ from difflib import SequenceMatcher
 
 
 def search_similar_str(text, substr):
-    leader = (0, set())
+    """Поиск наиболее похожей подстроки в тексте.
+    Принимает на вход текст и искомую подстроку.
+    Возвращает коэффициент схожести и список похожих подстрок"""
+    if substr in text:
+        return 1, [substr]
 
-    for window in (text[i:i + len(substr)] for i in range(len(text) - len(substr) + 1)):
+    leader = (0, set())
+    for window in (text[i:i + len(substr)] for i in
+                   range(len(text) - len(substr) + 1)):
         seq = SequenceMatcher(a=window, b=substr)
         score = seq.ratio()
         if score > leader[0]:
@@ -14,13 +20,13 @@ def search_similar_str(text, substr):
         elif score == leader[0]:
             leader[1].add(window)
         if leader[0] == 1:
-            return leader
+            return leader[0], list(leader[1])
         print(score, window, substr)
 
-    return leader
+    return leader[0], list(leader[1])
 
 
 if __name__ == '__main__':
     a = "ACATCTATGGACATTACCCC"
     b = "CTATGAGC"
-    print('Searched:', search_similar_str(a, b))
+    print('Searched:', *search_similar_str(a, b))
