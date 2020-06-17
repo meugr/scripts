3a94d025ae34f04d30b8112362b6efd3909a5fb9
meugr <meugr32@gmail.com>
2020-06-17 13:08:02 +0300

    upd

--- search_similas_substring.py
+++ search_similas_substring.py
@@ -3,24 +3,24 @@
 from difflib import SequenceMatcher
 
 
-def get_subs(seq, length):
-    """Generates all substrings of a given length"""
-    for i in range(len(seq) - length + 1):
-        yield seq[i: i + length]
+def search_similar_str(text, substr):
+    leader = (0, set())
+
+    for window in (text[i:i + len(substr)] for i in range(len(text) - len(substr) + 1)):
+        seq = SequenceMatcher(a=window, b=substr)
+        score = seq.ratio()
+        if score > leader[0]:
+            leader = (score, {window})
+        elif score == leader[0]:
+            leader[1].add(window)
+        if leader[0] == 1:
+            return leader
+        print(score, window, substr)
+
+    return leader
 
 
-a = "ACATCTATGGACATTACCCC"
-b = "CTATGAGC"
-leader = [0, set()]
-
-for window in get_subs(a, len(b)):
-    seq = SequenceMatcher(a=window, b=b)
-    score = seq.ratio()
-    if score > leader[0]:
-        leader = [score, {window}]
-    elif score == leader[0]:
-        leader[1].add(window)
-    print(score, window, b)
-print('======')
-print(leader)
-
+if __name__ == '__main__':
+    a = "ACATCTATGGACATTACCCC"
+    b = "CTATGAGC"
+    print('Searched:', search_similar_str(a, b))
